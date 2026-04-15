63230f8e991416a5fa09277ac28711a140c707f6
gray-ji <seeu314@gmail.com>
2026-04-15 14:13:01 +0900

    init commit

---
+++ src/main/java/com/sparta/crudcodeshadowing/CrudCodeShadowingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.crudcodeshadowing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCodeShadowingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudCodeShadowingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/crudcodeshadowing/CrudCodeShadowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.crudcodeshadowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCodeShadowingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
